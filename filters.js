add6461903898572e8b11faade207b62b54bd97c
k-allgood <48075080+k-allgood@users.noreply.github.com>
2019-05-15 21:16:37 -0400

    Updates to shoes.html, main.css & filters.js
    filters.js now adds the selection class to each li inside #filters. Created function to show appropriate class based on parent node id. Rewrote showFilters() so that all code except call was contained within function.

--- filters.js
+++ filters.js
@@ -1,19 +1,20 @@
 function showFilters() {
-	//Make sure .hidden listed after .flexy in CSS
-	const filterList = document.getElementById("filters").classList.toggle("flex");
-	const arrow = document.getElementById("arrow");
-	(arrow.textContent === "↓") ? arrow.textContent = "↑" : arrow.textContent = "↓";
+	//Make sure .hidden listed after .flex in CSS
+	const filterMenu = document.querySelector("#filtermenu");
+	filterMenu.addEventListener("click", function() {
+		const filterList = document.getElementById("filters").classList.toggle("flex");
+		const arrow = document.getElementById("arrow");
+		(arrow.textContent === "↓") ? arrow.textContent = "↑" : arrow.textContent = "↓";
+	});
 }
+showFilters();
 
-const filterMenu = document.querySelector("#filtermenu");
-filterMenu.addEventListener("click", function() {
-	showFilters();
-});
-
+//Returns an element with specified class
 function hasClass(elem, className) {
     return elem.classList.contains(className);
 }
 
+//Shows filter options based on which toggle was selected.
 function openFilter() {
 	document.querySelector("#filters").addEventListener("click", function (e) {
     if (hasClass(e.target, "toggle")) {
@@ -24,3 +25,32 @@ function openFilter() {
 	});
 }
 openFilter();
+
+function selected() {
+	document.querySelector("#filters").addEventListener("click", function(e) {
+		let items = document.querySelector("#filters").querySelectorAll("li");
+		//Grabs current iteration of loop (val)
+		//Add the .selection class to each li under #filters
+		items.forEach(function(val) {
+			val.classList.add("selection");
+		});
+		//Get the clicked li's parent's ID & toggle appropriate class
+		if (hasClass(e.target, "selection")) {
+			let parent = e.target.parentNode.id;
+			switch (parent) {
+				case "sizeList":
+					e.target.classList.toggle("clickedSize");
+					break;
+				case "priceList":
+					e.target.classList.toggle("clickedPrice");
+					break;
+				case "colorList":
+					e.target.classList.toggle("clickedColor");
+					break;
+				default:
+					break;
+			}
+		}
+	});
+}
+selected();
